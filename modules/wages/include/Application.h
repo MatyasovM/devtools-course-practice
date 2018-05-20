61b717ccc206449cb904186ed7d5d549bd8ffb90
Matyasov.Mixail <matyasov.mixail@gmail.com>
2018-05-20 19:27:20 +0300

    Add Application

---
+++ modules/wages/include/Application.h
@@ -0,0 +1,22 @@
+// Copyright 2018 Matyasov Mixail
+
+#pragma once
+#include"Wages.h"
+#include<iostream>
+#include<string>
+
+class Application {
+    static double calculatedValues;
+public:
+    Application() {}
+    static std::string DoubleToString(double val);
+    static double StringToDouble(char* str);
+    static Month IntToMonth(int val);
+    static double GetCalculatedValues() { return calculatedValues; }
+    static std::string CalculateWages(double salary, double administrativeLeaveHours, double overtime, Month month);
+    static std::string CalculateHourlyPay(double salary, double administrativeLeaveHours, double overtime, Month month);
+    static std::string CalculationWagesWithoutOvertime(double salary, double administrativeLeaveHours, double overtime, Month month);
+    static std::string CalculationOvertimePayment(double salary, double administrativeLeaveHours, double overtime, Month month);
+
+    std::string operator()(int argc, char** argv);
+};
---
+++ modules/wages/test/test_Application.cpp
@@ -0,0 +1,111 @@
+// Copyright 2018 Matyasov Mixail
+
+#include <gtest/gtest.h>
+#include"include/Application.h"
+
+TEST(Application, can_double_to_string) {
+    std::string str = "123.54";
+
+
+    EXPECT_TRUE(str.compare(Application::DoubleToString(123.54)));
+}
+
+TEST(Application, can_int_to_string) {
+    std::string str = "123";
+
+
+    EXPECT_TRUE(str.compare(Application::DoubleToString(123)));
+}
+
+TEST(Application, can_string_to_double) {
+    double val = 123.54;
+
+    EXPECT_EQ(val, Application::StringToDouble("123.54"));
+}
+
+TEST(Application, can_string_to_int) {
+    double val = 123;
+
+    EXPECT_EQ(val, Application::StringToDouble("123"));
+}
+
+TEST(Application, can_int_to_Month) {
+    Application::IntToMonth(1);
+    Application::IntToMonth(2);
+    Application::IntToMonth(3);
+    Application::IntToMonth(4);
+    Application::IntToMonth(5);
+    Application::IntToMonth(6);
+    Application::IntToMonth(7);
+    Application::IntToMonth(8);
+    Application::IntToMonth(9);
+    Application::IntToMonth(10);
+    Application::IntToMonth(11);
+    Application::IntToMonth(12);
+
+    EXPECT_EQ(January, Application::IntToMonth(1));
+}
+
+TEST(Application, can_CalculateWages) {
+    int argc = 6;
+    char** argv;
+    argv = new char*[argc];
+    argv[0] = "";
+    argv[1] = "CalculateWages";
+    argv[2] = "10000";
+    argv[3] = "0";
+    argv[4] = "0";
+    argv[5] = "0";
+    Application app;
+
+
+    ASSERT_NO_THROW(app(argc, argv));
+}
+
+TEST(Application, can_CalucateHourlyPay) {
+    int argc = 6;
+    char** argv;
+    argv = new char*[argc];
+    argv[0] = "";
+    argv[1] = "HourlyPay";
+    argv[2] = "10000";
+    argv[3] = "0";
+    argv[4] = "0";
+    argv[5] = "0";
+    Application app;
+
+
+    ASSERT_NO_THROW(app(argc, argv));
+}
+
+TEST(Application, can_CalculationOvertimePayment) {
+    int argc = 6;
+    char** argv;
+    argv = new char*[argc];
+    argv[0] = "";
+    argv[1] = "CalculationOvertimePayment";
+    argv[2] = "10000";
+    argv[3] = "0";
+    argv[4] = "0";
+    argv[5] = "0";
+    Application app;
+
+
+    ASSERT_NO_THROW(app(argc, argv));
+}
+
+TEST(Application, can_CalculationWagesWithoutOvertime) {
+    int argc = 6;
+    char** argv;
+    argv = new char*[argc];
+    argv[0] = "";
+    argv[1] = "CalculationWagesWithoutOvertime";
+    argv[2] = "10000";
+    argv[3] = "0";
+    argv[4] = "0";
+    argv[5] = "0";
+    Application app;
+
+
+    ASSERT_NO_THROW(app(argc, argv));
+}
