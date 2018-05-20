a242250bdab2172298baee6f3ffd48b2e897cafe
Matyasov.Mixail <matyasov.mixail@gmail.com>
2018-05-20 20:54:32 +0300

    Add Application

--- modules/wages/test/test_Application.cpp
+++ modules/wages/test/test_Application.cpp
@@ -5,45 +5,50 @@
 
 TEST(Application, can_double_to_string) {
     std::string str = "123.54";
+    Application app;
 
 
-    EXPECT_TRUE(str.compare(Application::DoubleToString(123.54)));
+    EXPECT_TRUE(str.compare(app.DoubleToString(123.54)));
 }
 
 TEST(Application, can_int_to_string) {
     std::string str = "123";
+    Application app;
 
 
-    EXPECT_TRUE(str.compare(Application::DoubleToString(123)));
+    EXPECT_TRUE(str.compare(app.DoubleToString(123)));
 }
 
 TEST(Application, can_string_to_double) {
     double val = 123.54;
+    Application app;
 
-    EXPECT_EQ(val, Application::StringToDouble((char*)"123.54"));
+    EXPECT_EQ(val, app.StringToDouble((char*)"123.54"));
 }
 
 TEST(Application, can_string_to_int) {
     double val = 123;
+    Application app;
 
-    EXPECT_EQ(val, Application::StringToDouble((char*)"123"));
+    EXPECT_EQ(val, app.StringToDouble((char*)"123"));
 }
 
 TEST(Application, can_int_to_Month) {
-    Application::IntToMonth(1);
-    Application::IntToMonth(2);
-    Application::IntToMonth(3);
-    Application::IntToMonth(4);
-    Application::IntToMonth(5);
-    Application::IntToMonth(6);
-    Application::IntToMonth(7);
-    Application::IntToMonth(8);
-    Application::IntToMonth(9);
-    Application::IntToMonth(10);
-    Application::IntToMonth(11);
-    Application::IntToMonth(12);
+    Application app;
+    app.IntToMonth(1);
+    app.IntToMonth(2);
+    app.IntToMonth(3);
+    app.IntToMonth(4);
+    app.IntToMonth(5);
+    app.IntToMonth(6);
+    app.IntToMonth(7);
+    app.IntToMonth(8);
+    app.IntToMonth(9);
+    app.IntToMonth(10);
+    app.IntToMonth(11);
+    app.IntToMonth(12);
 
-    EXPECT_EQ(January, Application::IntToMonth(1));
+    EXPECT_EQ(January, app.IntToMonth(1));
 }
 
 TEST(Application, can_CalculateWages) {
