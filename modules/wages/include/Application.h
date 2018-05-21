626879a07aeef8c7477d593e4ea964042662c8d9
Matyasov.Mixail <matyasov.mixail@gmail.com>
2018-05-21 17:14:43 +0300

    delete static

--- modules/wages/include/Application.h
+++ modules/wages/include/Application.h
@@ -11,14 +11,14 @@ class Application {
     static double calculatedValues;
 public:
     explicit Application(double val = 0);
-    static std::string DoubleToString(double val);
-    static double StringToDouble(const char* str);
-    static Month IntToMonth(int val);
-    static double GetCalculatedValues() { return calculatedValues; }
-    static std::string CalculateWages(double salary, double administrativeLeaveHours, double overtime, Month month);
-    static std::string CalculateHourlyPay(double salary, double administrativeLeaveHours, double overtime, Month month);
-    static std::string CalculationWagesWithoutOvertime(double salary, double administrativeLeaveHours, double overtime, Month month);
-    static std::string CalculationOvertimePayment(double salary, double administrativeLeaveHours, double overtime, Month month);
+    std::string DoubleToString(double val);
+    double StringToDouble(const char* str);
+    Month IntToMonth(int val);
+    double GetCalculatedValues() { return calculatedValues; }
+    std::string CalculateWages(double salary, double administrativeLeaveHours, double overtime, Month month);
+    std::string CalculateHourlyPay(double salary, double administrativeLeaveHours, double overtime, Month month);
+    std::string CalculationWagesWithoutOvertime(double salary, double administrativeLeaveHours, double overtime, Month month);
+    std::string CalculationOvertimePayment(double salary, double administrativeLeaveHours, double overtime, Month month);
 
     std::string operator()(int argc, char** argv);
 };
